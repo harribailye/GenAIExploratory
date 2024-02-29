573c6517eddb31ef3eed2b727136db1f806537af
Bailye <harrison.bailye@accenture.com>
2024-02-29 11:22:27 +1030

    Code is linked to OpenAI with API

---
+++ basicExplore.py
@@ -0,0 +1,22 @@
+# Import libaries 
+from openai import OpenAI
+import os
+import pandas as pd
+
+'''
+# Set API key 
+client = OpenAI(
+    api_key="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",
+)
+
+# Set up the model
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello"}
+  ]
+) 
+
+print(response.choices[0].message)
+'''
