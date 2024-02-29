7b2dfd1469cea0d7622ed6c9e71c6b3b3921da1e
Bailye <harrison.bailye@accenture.com>
2024-03-01 09:51:35 +1100

    Making code scan current directory for all audio/video files to convert

--- SummariseLecture.py
+++ SummariseLecture.py
@@ -6,39 +6,65 @@
 # Import libaries 
 from openai import OpenAI
 import moviepy.editor as mp
-import pandas as pd
 import speech_recognition as sr 
 import pyaudio
+import os 
 
 # Set API key 
 client = OpenAI(
     api_key="",
 ) 
 
-# Define the System Role
-messages = [ {"role": "system", "content": "Your response should summarise the meeting."} ]
+# Set up the System Role
+messages = [ {"role": "system", "content": "Summarise the meeting in one sentence."} ]
 
 # Set up microphone to record audio prompt 
 recogniser = sr.Recognizer()
 microphone = sr.Microphone(device_index= 0) # Change this according to your microphone
 
-# Define audio file
-lecture_audio = 'Lecture.wav'
-lecture_video = 'LectureVideo.MP4'
+# Get a list of files in current working directory 
+current_dir = os.getcwd()
+files_and_dirs = os.listdir(current_dir)
 
-# MP4 to text 
-video = mp.VideoFileClip(lecture_video)
-audio = video.audio
-temp_audio_file = "temp.wav"
-audio.write_audiofile(temp_audio_file)
+# Filter out only the mp4 and wav files
+files = [file for file in files_and_dirs if file.endswith('.mp4') or file.endswith('.wav') and os.path.isfile(os.path.join(current_dir, file))]
 
 
-# Convert audio file 
-with sr.AudioFile(temp_audio_file) as source:
-    audio_data = recogniser.record(source)
-    text = recogniser.recognize_google(audio_data)
-    messages.append({"role": "user", "content": text}) 
 
+# Audio to text function 
+def audio_2_text(temp_audio_file):
+    recogniser = sr.Recognizer()
+    messages = []
+
+    with sr.AudioFile(temp_audio_file) as source:
+        audio_data = recogniser.record(source)
+        text = recogniser.recognize_google(audio_data)
+        messages.append({"role": "user", "content": text}) 
+
+    return messages
+
+# Convert audio to text
+def convert_files(files):
+  
+  for file in files:
+    if file.endswith('4'):
+      video = mp.VideoFileClip(file)
+      audio = video.audio
+      temp_audio_file = "temp.wav"
+      audio.write_audiofile(temp_audio_file)
+      
+      text = audio_2_text(temp_audio_file) 
+      print(text)
+    else: 
+      text = audio_2_text(file) 
+      print(text)
+     
+
+print(convert_files(files))
+
+
+
+'''
 # Set up the model and send the user prompt
 response = client.chat.completions.create(
   model="gpt-3.5-turbo",
@@ -47,3 +73,4 @@ response = client.chat.completions.create(
 
 # Print the model output 
 print(response.choices[0].message)
+'''
